27f53f3a0ab7d75882accc62af56f09f8cfaff4e
rthakur1312 <67025716+rthakur1312@users.noreply.github.com>
2020-11-18 19:30:10 -0500

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -5,10 +5,10 @@
 // selects the database, and sets the encoding.
 
 // Set the database access information as constants:
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'p1RashiThakur');
+define('DB_USER', 'xwyk2lftsodx5ffs');
+define('DB_PASSWORD', 'hyn1ujyz1sfxio3l');
+define('DB_HOST', 'cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_NAME', 'xirdnub0067fny84');
 
 // Make the connection:
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MySQL: ' . mysqli_connect_error() );
